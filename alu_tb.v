3cce64f7fe4d587d6e8c2fcdffabbaf65175a355
Vadim Davydov <vadim.davydov77@gmail.com>
2026-05-01 15:26:38 -0400

    Move tb to separate file

---
+++ alu_tb.v
@@ -0,0 +1,25 @@
+module alu_tb;
+
+    // Define I/O
+    reg [7:0] A, B;  // TODO: understand why I - reg, O - wire
+
+    reg  [2:0] opcode;
+    wire [7:0] result;
+    wire [3:0] flags;
+
+    // instantiate alu
+    ALU_8bit uut (
+        .A(A),
+        .B(B),
+        .opcode(opcode),
+        .result(result),
+        .flags(flags)
+    );
+
+    initial begin
+        $dumpfile("alu_test.vcd");
+        $dumpvars(0, alu_tb);  // ?
+    end
+
+
+endmodule
